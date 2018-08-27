05bb072846b408f35ec1fa585a811f7ddbb7ba59
Keith <keithwhisper@gmail.com>
2018-08-27 20:09:16 +0800

    Publish
     - @nervos/chain@0.17.11
     - neuron-web@0.1.1

--- packages/neuron-web/src/utils/compActions.tsx
+++
@@ -1,8 +0,0 @@
-import { ChangeEvent } from 'react'
-export const handleInputOf = (ctx: any) => (key: string) => (e: ChangeEvent<HTMLInputElement>) => {
-  const { value } = e.currentTarget as any
-  ctx.setState({
-    [key]: value,
-    [`${key}Error`]: '',
-  })
-}
