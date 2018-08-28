2c0fb1b9f546f2f26055d7b1856c0a99f45f96f3
Keith <keithwhisper@gmail.com>
2018-08-28 17:52:48 +0800

    fix: fix hex to bytes

---
+++ packages/nervos-chain/src/utils/patchHexToBytes.ts
@@ -0,0 +1,13 @@
+export default (web3: any) => {
+  // web3.utils.hexToBytes = hex2bytes
+  web3.utils.hexToBytes = new Proxy(web3.utils.hexToBytes, {
+    apply: (target: Function, _thisArg: any, argumentsList: string[]) => {
+      let hex = ('' + argumentsList[0] || '').replace(/^0x/, '')
+      console.log(hex)
+      if (hex.length % 2) {
+        hex = '0' + hex
+      }
+      return target('0x' + hex)
+    }
+  })
+}
