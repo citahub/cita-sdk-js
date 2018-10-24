b3c8719307332c434c9d1fa43206d116789ea92a
Keith <keithwhisper@gmail.com>
2018-10-24 11:05:30 +0800

    update: build for release

--- packages/nervos-chain/lib/systems/abis/batchTx.js
+++ packages/nervos-chain/lib/systems/abis/batchTx.js
@@ -1,3 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = [{ "constant": false, "inputs": [{ "name": "", "type": "bytes" }], "name": "multiTxs", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }];
+exports.default = [
+    {
+        constant: false,
+        inputs: [{ name: '', type: 'bytes' }],
+        name: 'multiTxs',
+        outputs: [],
+        payable: false,
+        stateMutability: 'nonpayable',
+        type: 'function'
+    }
+];
