78b2e123c668846cda9d927e169e8818fe6b0d06
Chen Yu <keithwhisper@gmail.com>
2018-11-13 01:11:24 +0800

    remove protobuf.toObject (#76)
    * update: rename nervos to appchain
    
    * Update/rename to appchain (#60)
    
    * update: rename nervos to appchain
    
    * update: remove package-lock
    
    * v0.18.2
    
    * update: update signer tests with appchain/base
    
    * update readme of appchain-base[skip ci]
    
    * update: update protobuf
    
    * Change travis repo (#62)
    
    * Change Travis repo
    
    * Update  to
    
    * update: support cita proto v1
    
    * update: remove nervos-chain
    
    * update: update signature methods with upgrade of protobuf
    
    * update: remove test for cita v1
    
    * update: add contract.getPastEvents
    
    * Publish
    
     - @appchain/base@0.19.0
     - @appchain/signer@1.0.39
     - neuron-web@0.0.2
    
    * Publish
    
     - @appchain/base@0.19.1
     - neuron-web@0.0.3
    
    * Feature/proto buf v1 (#65)
    
    * update: update protobuf
    
    * Feature/sign with api version (#64)
    
    * update: support cita proto v1
    
    * update: remove nervos-chain
    
    * update: update travis badges
    
    * build
    
    * update: build for release
    
    * update: add formatter to getTransaction
    
    * update: remove var declaration
    
    * update: build for release
    
    * update: refactor store abi method
    
    * update: add example/config to gitignore
    
    * update: refactor store abi method
    
    * Update/refactor neuron web (#71)
    
    * update: add formatter to getTransaction
    
    * update: refactor neuron-web
    
    * update: add appchain-tests
    
    * update: build for release
    
    * update: build for release appchain/base bundle.js
    
    *  fix: add 0x formatted to in result of unsigner
    
    * update: update unsigner mode (#75)
    
    * update: build

--- packages/appchain-signer/lib/unsigner.js
+++ packages/appchain-signer/lib/unsigner.js
@@ -3,30 +3,33 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Signature = require('elliptic/lib/elliptic/ec/signature');
 const blockchainPb = require('../proto-js/blockchain_pb');
 const index_1 = require("./index");
-const base64ToHex = (b64) => Buffer.from(b64, 'base64')
-    .toString('hex')
-    .replace(/^0+/, '');
 const unsigner = (hexUnverifiedTransaction) => {
     const bytesUnverifiedTransaction = index_1.hex2bytes(hexUnverifiedTransaction);
     const unverifiedTransaction = blockchainPb.UnverifiedTransaction.deserializeBinary(bytesUnverifiedTransaction);
     const transactionPb = unverifiedTransaction.getTransaction();
     const signature = unverifiedTransaction.getSignature();
-    const transaction = blockchainPb.Transaction.toObject(true, transactionPb);
-    transaction.value = base64ToHex(transaction.value);
-    transaction.data = base64ToHex(transaction.data);
-    transaction.value = transaction.value ? '0x' + transaction.value : `0x0`;
-    transaction.data = transaction.data ? '0x' + transaction.data : transaction.data;
-    switch (+transaction.version) {
+    const version = transactionPb.getVersion();
+    const transaction = {
+        version,
+        data: index_1.bytes2hex(transactionPb.getData()),
+        chainId: transactionPb.getChainId(),
+        to: transactionPb.getTo(),
+        nonce: transactionPb.getNonce(),
+        value: +index_1.bytes2hex(transactionPb.getValue()),
+        quota: transactionPb.getQuota(),
+        validUntilBlock: +transactionPb.getValidUntilBlock(),
+    };
+    switch (+version) {
         case 1: {
-            transaction.chainId = base64ToHex(transaction.chainIdV1);
-            transaction.to = transaction.toV1;
-            delete transaction.chainIdV1;
-            delete transaction.toV1;
+            transaction.chainId = '0x' + (+index_1.bytes2hex(transactionPb.getChainIdV1())).toString(16);
+            transaction.to = index_1.bytes2hex(transactionPb.getToV1());
             break;
         }
         default: {
-            break;
         }
+    }
+    if (transaction.to) {
+        transaction.to = '0x' + transaction.to;
     }
     const sign = new Signature({
         r: index_1.bytes2hex(signature.slice(0, 32)).slice(2),
