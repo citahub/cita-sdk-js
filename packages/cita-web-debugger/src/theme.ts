cbee164ab496df4ec2ae6a72bfc2d09f80b400f1
Keith <keithwhisper@gmail.com>
2019-10-22 18:57:12 +0800

    feat: remove the cita-web-debugger

--- packages/cita-web-debugger/src/theme.ts
+++
@@ -1,8 +0,0 @@
-import blue from '@material-ui/core/colors/blue'
-import { createMuiTheme } from '@material-ui/core/styles'
-
-export default createMuiTheme({
-  palette: {
-    primary: blue,
-  },
-})
